a161184cdae731265ead2aa13bbfbf5c376a0718
Ceausescu <andrei-cristian.ceausescu@haufe-lexware.com>
2019-07-02 15:06:44 +0300

    update style

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,8 +1,22 @@
 var React = require('react');
+var { Link } = require('react-router');
 
 var Examples = () => {
     return (
-        <h3>Examples</h3>
+        <div>
+            <h1 className="text-centered">Examples</h1>
+            <p>Here are a few example location to try</p>
+            <ol>
+                <li>
+                    <Link to='/?location=Timisoara'> Timisoara, TM</Link>
+                </li>
+                <li>
+                    <Link to='/?location=Rio'>Rio, Brazil</Link>
+                </li>
+            </ol>
+
+        </div>
+
     );
 }
 
